e18f805f4820286b76a919c2b8edf3b33e04fa64
lunahobi <muradyan.lusine.05@mail.ru>
2023-04-06 22:10:24 +0300

    Добавила цикл while

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,34 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Stack stack = new Stack();
         Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        char[] a = s.toCharArray();
-        boolean brackets = true;
-        for (int i = 0; i < a.length; i++){
-            if ((a[i] == '(') || (a[i] == '{') || a[i] == '[') {
-                stack.push(a[i]);
+        while (true) {
+            Stack stack = new Stack();
+            String s = in.nextLine();
+            char[] a = s.toCharArray();
+            boolean brackets = true;
+            for (int i = 0; i < a.length; i++) {
+                if ((a[i] == '(') || (a[i] == '{') || a[i] == '[') {
+                    stack.push(a[i]);
+                } else if ((!stack.empty()) && (a[i] == ')' || a[i] == '}' || a[i] == ']')) {
+                    if ((a[i] == ')' && stack.top() == '(') || ((a[i] == ']' && stack.top() == '[') || (a[i] == '}' && stack.top() == '{'))) {
+                        stack.pop();
+                    } else {
+                        brackets = false;
+                        break;
+                    }
+                } else if ((stack.empty()) && (a[i] == ')' || a[i] == '}' || a[i] == ']')) {
+                    brackets = false;
+                    break;
+                }
             }
-            else if ((!stack.empty()) && (a[i] == ')' || a[i] == '}' || a[i] == ']')) {
-               if ((a[i] == ')' && stack.top() == '(') || ((a[i] == ']' && stack.top() =='[') || (a[i] == '}' && stack.top() == '{'))){
-                   stack.pop();
-               }
-               else{
-                   brackets = false;
-                   break;
-               }
-            }
-            else if ((stack.empty()) && (a[i] == ')' || a[i] == '}' || a[i] == ']')) {
-                brackets = false;
-                break;
-            }
+            if (brackets && stack.empty())
+                System.out.println("true");
+            else
+                System.out.println("false");
+
         }
-        if (brackets && stack.empty()){
-            System.out.println("true");
-        }
-        else{
-            System.out.println("false");
-        }
+
     }
 }
